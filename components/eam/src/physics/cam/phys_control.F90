5f256b9606f8b2751ed66fc1715cbbe32e86f336
Hui Wan from Cori/Edison <hui.wan@pnnl.gov>
2024-02-13 09:38:18 -0800

    add namelist variable ndrop_cldf_growth_threshold
    The default value is set to 0.01 so that an out-of-the-box
    run (which has regen_fix = .true.) will still give BFB results.
    
    But this new namelist variable allows us to use Dick's new
    threshold of 0.001 when setting regen_fix = .false.

--- components/eam/src/physics/cam/phys_control.F90
+++ components/eam/src/physics/cam/phys_control.F90
@@ -98,6 +98,7 @@ real(r8)          :: n_so4_monolayers_pcage = huge(1.0_r8) ! number of so4(+nh4)
 real(r8)          :: micro_mg_accre_enhan_fac = huge(1.0_r8) !!Accretion enhancement factor
 logical           :: liqcf_fix            = .false.    ! liq cld fraction fix calc.                     
 logical           :: regen_fix            = .false.    ! aerosol regeneration bug fix for ndrop.F90 
+real(r8)          :: ndrop_cldf_growth_threshold = 0.01_r8 ! cloud fraction increment threshold for considering cloud growth in ndrop.F90
 logical           :: demott_ice_nuc       = .false.    ! use DeMott ice nucleation treatment in microphysics 
 logical           :: pergro_mods          = .false.    ! for invoking pergro related changes in the code
 logical           :: pergro_test_active   = .false.    ! for invoking pergro test
@@ -207,6 +208,7 @@ subroutine phys_ctl_readnl(nlfile)
       cld_macmic_num_steps, micro_do_icesupersat, &
       fix_g1_err_ndrop, ssalt_tuning, resus_fix, convproc_do_aer, &
       convproc_do_gas, convproc_method_activate, liqcf_fix, regen_fix, demott_ice_nuc, pergro_mods, pergro_test_active, &
+      ndrop_cldf_growth_threshold, &
       mam_amicphys_optaa, n_so4_monolayers_pcage,micro_mg_accre_enhan_fac, &
       l_tracer_aero, l_vdiff, l_rayleigh, l_gw_drag, l_ac_energy_chk, &
       l_bc_energy_fix, l_dry_adj, l_st_mac, l_st_mic, l_rad, prc_coef1,prc_exp,prc_exp1,cld_sed,mg_prc_coeff_fix, &
@@ -288,6 +290,7 @@ subroutine phys_ctl_readnl(nlfile)
    call mpibcast(micro_mg_accre_enhan_fac,        1 , mpir8,   0, mpicom)
    call mpibcast(liqcf_fix,                       1 , mpilog,  0, mpicom)
    call mpibcast(regen_fix,                       1 , mpilog,  0, mpicom)
+   call mpibcast(ndrop_cldf_growth_threshold,     1 , mpir8,   0, mpicom)
    call mpibcast(demott_ice_nuc,                  1 , mpilog,  0, mpicom)
    call mpibcast(pergro_mods,                     1 , mpilog,  0, mpicom)
    call mpibcast(pergro_test_active,              1 , mpilog,  0, mpicom)
@@ -471,6 +474,7 @@ subroutine phys_getopts(deep_scheme_out, shallow_scheme_out, eddy_scheme_out, &
                         fix_g1_err_ndrop_out, ssalt_tuning_out,resus_fix_out,convproc_do_aer_out,  &
                         convproc_do_gas_out, convproc_method_activate_out, mam_amicphys_optaa_out, n_so4_monolayers_pcage_out, &
                         micro_mg_accre_enhan_fac_out, liqcf_fix_out, regen_fix_out,demott_ice_nuc_out, pergro_mods_out, pergro_test_active_out &
+                       ,ndrop_cldf_growth_threshold_out &
                        ,l_tracer_aero_out, l_vdiff_out, l_rayleigh_out, l_gw_drag_out, l_ac_energy_chk_out  &
                        ,l_bc_energy_fix_out, l_dry_adj_out, l_st_mac_out, l_st_mic_out, l_rad_out  &
                        ,prc_coef1_out,prc_exp_out,prc_exp1_out, cld_sed_out,mg_prc_coeff_fix_out,rrtmg_temp_fix_out)
@@ -536,6 +540,7 @@ subroutine phys_getopts(deep_scheme_out, shallow_scheme_out, eddy_scheme_out, &
    real(r8),          intent(out), optional :: micro_mg_accre_enhan_fac_out
    logical,           intent(out), optional :: liqcf_fix_out       
    logical,           intent(out), optional :: regen_fix_out       
+   real(r8),          intent(out), optional :: ndrop_cldf_growth_threshold_out       
    logical,           intent(out), optional :: demott_ice_nuc_out  
    logical,           intent(out), optional :: pergro_mods_out     
    logical,           intent(out), optional :: pergro_test_active_out     
@@ -614,6 +619,7 @@ subroutine phys_getopts(deep_scheme_out, shallow_scheme_out, eddy_scheme_out, &
    if ( present(micro_mg_accre_enhan_fac_out)) micro_mg_accre_enhan_fac_out = micro_mg_accre_enhan_fac
    if ( present(liqcf_fix_out           ) ) liqcf_fix_out            = liqcf_fix      
    if ( present(regen_fix_out           ) ) regen_fix_out            = regen_fix      
+   if ( present(ndrop_cldf_growth_threshold_out ) ) ndrop_cldf_growth_threshold_out = ndrop_cldf_growth_threshold      
    if ( present(demott_ice_nuc_out      ) ) demott_ice_nuc_out       = demott_ice_nuc 
    if ( present(pergro_mods_out         ) ) pergro_mods_out          = pergro_mods
    if ( present(pergro_test_active_out  ) ) pergro_test_active_out   = pergro_test_active
